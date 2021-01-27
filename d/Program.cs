7e81f216312eb147fd5a5e3cb7dd187ad68eb76b
Nollili <66601688+Nollili@users.noreply.github.com>
2021-01-27 10:46:54 +0100

    Add files via upload

---
+++ d/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace last
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Bye");
+
+            }
+
+        }
+    }
+}
