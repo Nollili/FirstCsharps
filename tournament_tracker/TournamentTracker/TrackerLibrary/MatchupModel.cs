a99c6e12a7d00671629a3cb39d2c769c619f3cfb
Nollili <noemilili@gmail.com>
2021-05-01 16:51:57 +0200

    w

---
+++ tournament_tracker/TournamentTracker/TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Represents one team in the matchup
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        /// <summary>
+        /// Represents the score for this particular team
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this team came from as the winner
+        /// </summary>
+        public int MatchupRound { get; set; }
+    }
+}
